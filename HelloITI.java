ea4cb1e019439401ef50814694cb15628ceb9f9f
nourallah3 <imncro7@gmail.com>
2023-07-21 07:39:06 +0200

    add printHelloGit to HelloITI classn

--- HelloITI.java
+++ HelloITI.java
@@ -2,7 +2,11 @@ class HelloITI{
 	static void printHelloITI(){
 		System.out.println("Hello ITI");
 	}
+	static void printHelloGit(){
+		System.out.println("Hello Git");
+	}
 	public static void main(String[] args){
 		printHelloITI();
+		printHelloGit();
 	}
 }
