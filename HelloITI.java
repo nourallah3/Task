465265dc6dd4ca7108d523449d772b7a25d01e7d
nourallah3 <imncro7@gmail.com>
2023-07-21 07:35:18 +0200

    add HelloITI.class

--- HelloITI.java
+++ HelloITI.java
@@ -2,7 +2,7 @@ class HelloITI{
 	static void printHelloITI(){
 		System.out.println("Hello ITI");
 	}
-	public static void main(String() args){
+	public static void main(String[] args){
 		printHelloITI();
 	}
 }
